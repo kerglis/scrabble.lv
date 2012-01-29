e96ad680bb062471ce2a1d291cef703e9aa0dc24
Kristaps Erglis <kristaps.erglis@tieto.com>
2012-01-29 22:56:24 +0200

    Initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScrabbleLv::Application.config.secret_token = '495499a5e71e7410ab7ed902c4ff2937b07a7d38244dab92ad868b35d71d661ffb8b34bf7ba25fe12aa06c96c08b7b3416c591a69f7041af2a409e0c8db0b425'
