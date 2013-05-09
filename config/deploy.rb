491dc8408531e0cf7fca3afb23340752e8bec3ce
Kristaps Erglis <kristaps.erglis@gmail.com>
2013-05-09 21:31:00 +0300

    depoy latest

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,9 @@ set :application, "scrabble"
 set :repository, "git@bitbucket.org:kristaps_erglis/scrabble.lv.git"
 set :scm, :git
 set :deploy_via, :remote_cache
+
 set :rvm_type, :system
+set :rvm_path, "/usr/local/rvm"
 
 set :stages, %w{demo production}
 set :default_stage, "demo"
