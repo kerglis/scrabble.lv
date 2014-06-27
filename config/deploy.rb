e8061f836f5fd4bc1bd572a4c2e7b8e1baa6d090
Kristaps Erglis <kristaps.erglis@gmail.com>
2014-06-27 22:34:42 +0300

    deploy fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,10 +14,17 @@ require "capistrano/ext/multistage"
 
 set :keep_releases, 5
 after "deploy:update_code", "deploy:migrate"
+after "deploy", "symlinks:tmp"
 after "deploy", "deploy:cleanup"
 
 namespace :deploy do
   task :restart, roles: :app do
     run "touch #{deploy_to}/shared/tmp/restart.txt"
+  end
+end
+
+namespace :symlinks do
+  task :tmp do
+    run "rm -rf #{current_path}/tmp && ln -s #{deploy_to}/shared/tmp #{current_path}/tmp"
   end
 end
