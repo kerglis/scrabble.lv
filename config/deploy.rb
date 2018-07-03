7d079a025dd8bdf747e0abf53d20c42080c20b80
Kristaps Erglis <kristaps.erglis@gmail.com>
2018-07-03 21:24:38 +0300

    Update deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :application, 'scrabble'
 set :repo_url, 'git@github.com:kerglis/scrabble.lv.git'
 set :log_level, :info
 set :linked_files, %w{ tmp/restart.txt }
-set :linked_dirs, %w{ bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads }
+set :linked_dirs, %w{ bin log tmp/pids tmp/cache tmp/sockets public/system public/uploads }
 
 set :rvm_ruby_version, '2.3.7@scrabble'
 set :passenger_restart_with_touch, true
