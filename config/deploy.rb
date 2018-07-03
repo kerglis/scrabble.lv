89184e2e3e4a644196da13792ac9b6dc3eb8c287
Kristaps Erglis <kristaps.erglis@gmail.com>
2018-07-03 21:34:43 +0300

    Update config/deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,5 +8,3 @@ set :linked_dirs, %w{ bin log tmp/pids tmp/cache tmp/sockets public/system publi
 
 set :rvm_ruby_version, '2.3.7@scrabble'
 set :passenger_restart_with_touch, true
-
-append :linked_dirs, '.bundle'
